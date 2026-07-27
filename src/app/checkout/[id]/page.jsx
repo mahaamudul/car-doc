d779c343b61914c6a96f5987870a8db9ae687714
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-07-27 18:36:00 +0600

    heading banner error solve due to build

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,6 +1,6 @@
 import { getServiceDetails } from "@/services/api_call/getServices";
-import ServiceBanner from "@/components/serviceDetails/ServiceBanner";
 import CheckoutForm from "@/components/checkout/CheckoutForm";
+import Heading from "@/components/Shared/Heading";
 
 const Page = async ({ params }) => {
   const { id } = await params;
@@ -10,7 +10,7 @@ const Page = async ({ params }) => {
   return (
     <section className="pb-24">
       <div className="max-w-7xl mx-auto px-4 mt-10">
-        <ServiceBanner title="Check Out" />
+        <Heading title="Checkout from here" currentRoute="Checkout" />
       </div>
   
       <div className="max-w-7xl mx-auto px-4 mt-20">
